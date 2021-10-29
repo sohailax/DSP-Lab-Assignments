b76f16a3dc66c4527abfd4e99378c63f735bddf0
sohailax <93394651+sohailax@users.noreply.github.com>
2021-10-29 23:02:15 +0200

    Add files via upload

---
+++ Sohaila_dspLab.m
@@ -0,0 +1,40 @@
+%Sohaila Mohammed Farouq  Group: 7A
+
+
+a = [ -7  5  -9 ; 2  -1  2 ; 1  -1  2];
+
+b = [ 16  3  2  13 ; 5 10 11 8  ; 9  6  7  12 ;  4  15  14 1];
+
+c = [ 4  2  -3  ;  7 -7  9  ;  3  5  6 ];
+
+d = [ 6  3  2  ;  2 12  -7  ;  -1 6  2  ; -5 15 11];
+
+3*a - 5*c
+% 7*a + 2*b   error 
+c*a
+
+c*d'
+
+zeros(5)
+zeros(4,3)
+ones (5)
+ones (3,4)
+size(d)
+zeros (size(d))
+diag([1 2 3 4])
+eye(5)
+
+% [a,b] is error
+
+% [a;b] is error
+
+
+%Matrix 7*8
+i=[5;5;5;5;5;5;5];
+j=diag ([5 5 5 5 5 5 5]);
+x = [j , i]
+
+
+a(2,:)
+
+a(:,1)
